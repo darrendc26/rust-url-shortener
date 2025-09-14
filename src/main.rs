1a9711f460f1192cb72951707c3dac282980eed7
Darren <darren26feb@gmail.com>
2025-09-14 12:37:40 +0530

    Create short url

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod short_url;
+use crate::short_url::short_url;
+use axum::{
+    Router,
+    extract::Path,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/shorten", post(short_url));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running at http://{}", addr);
+
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
+        .await
+        .unwrap();
+}
+
+async fn index() -> impl IntoResponse {
+    "Hello, world!"
+}
---
+++ src/short_url.rs
@@ -0,0 +1,19 @@
+use axum::{Json, http::StatusCode, response::IntoResponse};
+use rand::Rng;
+use serde::Deserialize;
+#[derive(Deserialize)]
+pub struct ShortenRequest {
+    url: String,
+}
+
+pub async fn short_url(Json(payload): Json<ShortenRequest>) -> impl IntoResponse {
+    let mut rng = rand::rng();
+    let mut short_url = "".to_string();
+    for _ in 0..7 {
+        let string = rng.sample(rand::distr::Alphanumeric) as char;
+        short_url += &string.to_string();
+    }
+    let shortened = format!("https://www.example.com/{}", short_url);
+
+    (StatusCode::OK, shortened)
+}
