0aa6b182e9ee71daa6253445a71991e8146b6fb9
Darren <darren26feb@gmail.com>
2025-09-19 06:20:17 +0530

    saved to db + fixes

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ mod short_url;
 use crate::short_url::short_url;
 use axum::{
     Router,
-    extract::Path,
-    http::StatusCode,
+    // extract::Path,
+    // http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
 };
+use dotenv::dotenv;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let pool = sqlx::PgPool::connect(&std::env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
     let app = Router::new()
         .route("/", get(index))
-        .route("/shorten", post(short_url));
+        .route("/shorten", post(short_url))
+        .with_state(pool);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server running at http://{}", addr);
--- src/short_url.rs
+++ src/short_url.rs
@@ -1,21 +1,43 @@
-use axum::{Json, http::StatusCode, response::IntoResponse};
-use rand::Rng;
-use serde::Deserialize;
-#[derive(Deserialize)]
+use axum::{Json, extract::State, http::StatusCode};
+use nanoid::nanoid;
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+
+#[derive(Deserialize, Debug)]
 pub struct ShortenRequest {
     url: String,
 }
 
-pub async fn short_url(Json(payload): Json<ShortenRequest>) -> impl IntoResponse {
+#[derive(Serialize, Debug)]
+pub struct ShortenResponse {
+    id: i32,
+    short_url: String,
+}
+
+pub async fn short_url(
+    State(pool): State<PgPool>,
+    Json(payload): Json<ShortenRequest>,
+) -> Result<(StatusCode, Json<ShortenResponse>), (StatusCode, Json<serde_json::Value>)> {
     let url = payload.url;
 
-    let mut rng = rand::rng();
-    let mut short_url = "".to_string();
-    for _ in 0..7 {
-        let string = rng.sample(rand::distr::Alphanumeric) as char;
-        short_url += &string.to_string();
-    }
-    let shortened = format!("https://www.example.com/{}", short_url);
+    println!("Received URL: {}", url);
 
-    (StatusCode::OK, shortened)
+    let shortened = nanoid!(7);
+
+    let result = sqlx::query!(
+        "INSERT INTO urls (url, short_url) VALUES ($1, $2) RETURNING id, short_url",
+        url,
+        shortened
+    )
+    .fetch_one(&pool)
+    .await
+    .unwrap();
+
+    Ok((
+        StatusCode::CREATED,
+        Json(ShortenResponse {
+            id: result.id,
+            short_url: result.short_url,
+        }),
+    ))
 }
